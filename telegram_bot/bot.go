ca34d7265e7aa7bea6199979e5bb51e473ea2a86
Laacis <109668309+Laacis@users.noreply.github.com>
2024-07-14 17:27:11 +0800

    bot now can handle 3 commands and reads .env file

--- telegram_bot/bot.go
+++ telegram_bot/bot.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("running...")
+	fmt.Println("V1.0")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatalf("TELEGRAM_BOT_TOKEN must be set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /generate and /status."
+		case "generate":
+			msg.Text = "executing generate orders..."
+			// call order-generation-service
+			orders, err := callOrderGenerationService()
+			if err != nil {
+				log.Println("Error generating order:", err)
+				continue
+			}
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, orders)
+		case "status":
+			msg.Text = "Server status: "
+		default:
+			msg.Text = "Unknown command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func callOrderGenerationService() (string, error) {
+	return string("Response DATA"), nil
 }

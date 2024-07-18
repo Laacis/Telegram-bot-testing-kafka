0e9e296674081731ee035103f890b6529f3cb228
Laacis <109668309+Laacis@users.noreply.github.com>
2024-07-18 19:19:43 +0800

    refactor Order struct

--- order_generation_service/models/order.go
+++ order_generation_service/models/order.go
@@ -6,11 +6,13 @@ import (
 )
 
 type Order struct {
-	OrderId      uuid.UUID `json:"order_id"`
-	CreationTime time.Time `json:"creation_time"`
-	CustomerId   uuid.UUID `json:"customer_id"`
-	Products     []string  `json:"products"`
-	//Products        []Product `json:"products"`
-	Delivery bool `json:"delivery"`
-	//DeliveryAddress Address   `json:"delivery_address"`
+	OrderId         uuid.UUID `json:"order_id"`
+	CreationTime    time.Time `json:"creation_time"`
+	CustomerId      int       `json:"customer_id"`
+	RestaurantCode  string    `json:"restaurant_code"`
+	RestaurantName  string    `json:"restaurant_name"`
+	Products        []Product `json:"products"`
+	IsDelivery      bool      `json:"isDelivery"`
+	DeliveryAddress string    `json:"delivery_address"`
+	BillingAddress  string    `json:"billing_address"`
 }

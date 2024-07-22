bfcc993c3002b1e8b0b5249976373917b990d21d
Laacis <109668309+Laacis@users.noreply.github.com>
2024-07-22 22:41:35 +0800

    init inMemoryStorage

---
+++ order_generation_service/services/storage/inMemoryStorage.go
@@ -0,0 +1,29 @@
+package order_generation_service
+
+type InMemoryStorageInterface[T any] interface {
+	Add(item T)
+	Length() int
+	AllRecords() []T
+}
+
+type InMemoryStorage[T any] struct {
+	items []T
+}
+
+func NewInMemoryStorage[T any]() *InMemoryStorage[T] {
+	return &InMemoryStorage[T]{
+		items: make([]T, 0),
+	}
+}
+
+func (s *InMemoryStorage[T]) Add(item T) {
+	s.items = append(s.items, item)
+}
+
+func (s *InMemoryStorage[T]) AllRecords() []T {
+	return s.items
+}
+
+func (s *InMemoryStorage[T]) Length() int {
+	return len(s.items)
+}

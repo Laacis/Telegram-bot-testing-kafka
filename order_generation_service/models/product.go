c58a0c49bd937c13616eb91ce5ee871bd81fd817
Laacis <109668309+Laacis@users.noreply.github.com>
2024-07-24 23:21:27 +0800

    refactor  product removed redundant id field
    the product is identified by productKey

--- order_generation_service/models/product.go
+++ order_generation_service/models/product.go
@@ -1,7 +1,6 @@
 package order_generation_service
 
 type Product struct {
-	Id              int     `json:"id"`
 	ProductKey      string  `json:"productKey"`
 	Name            string  `json:"name"`
 	Manufacturer    string  `json:"manufacturer"`

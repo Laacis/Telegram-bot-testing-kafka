2acaba4a4162127bbeb67012d059bec0d2b57786
Laacis <109668309+Laacis@users.noreply.github.com>
2024-07-28 18:09:20 +0800

    init kafka service producer

---
+++ kafka_manager/services/producer/producer.go
@@ -0,0 +1,20 @@
+package kafka_manager
+
+import "github.com/IBM/sarama"
+
+type KafkaProducer struct {
+	producer sarama.SyncProducer
+}
+
+func NewKafkaProducer(brokers []string) (*KafkaProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+	return &KafkaProducer{producer: producer}, nil
+}
